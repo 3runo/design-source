a13f8346af78130aed6697fdfe31103c58acead0
3runo <bruno.adex@gmail.com>
2020-01-21 21:03:03 +0100

    Improving folder structure confguration

--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -3,5 +3,5 @@ import * as React from 'react';
 type ButtonProps = { children: any };
 
 export default function Button(props: ButtonProps): JSX.Element {
-  return <button>{props.children}</button>;
+  return <button type="button">{props.children}</button>;
 }
